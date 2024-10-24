3e0a155db277b7e8f010656309d8be2225182799
Chigeru <frankjensen156@gmail.com>
2024-10-24 14:33:08 +0200

    Test af signin api

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 from .routers import read_item, skrald,users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(read_item.router)
 app.include_router(skrald.router)
 app.include_router(users.router)
+
+origins = [
+    "http://localhost:3000",  # React Native Metro bundler port
+    "http://127.0.0.1:3000",
+    "http://192.168.1.100:3000",  # Android device IP
+    "http://10.0.2.15:3000",     # iOS simulator IP
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
--- fastapiVenv/Scripts/activate.bat
+++ fastapiVenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Skole\Api\BookingCeption\fastapiVenv
+set VIRTUAL_ENV=C:\Users\Frank\Desktop\projects\BookingCeption\fastapiVenv
 
 if not defined PROMPT set PROMPT=$P$G
 
